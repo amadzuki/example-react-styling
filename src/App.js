8aa90df1821e4ea79678829491e9da56ea8d9a10
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-04-26 16:14:12 +0700

    Use theming with xstyled
    Co-authored-by: M Haidar Hanif <git@mhaidarhanif.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from "react"
+import styled from "@xstyled/styled-components"
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 11;
+  border: 2;
+  border-color: primary;
+  color: primary;
+  margin: 0 3;
+  padding: 1 3;
+`
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <div>
+      <Button>Try me</Button>
+    </div>
+  )
 }
 export default App
